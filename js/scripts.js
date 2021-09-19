30e1203b0cf9b7d67496985e0ad98c07155c8ac5
wdmuthiora <wdmuthiora@gmail.com>
2021-09-19 12:28:26 +0300

    Added JS to toggle the What We Do  icons and their respective descriptions.

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// Design description toggle
+$(document).ready(function () {
+
+  $("img.design").click(function () {
+
+    $("#design-description").toggle()
+    $(".design").hide()
+  })
+
+})
+
+// Development desction toggle
+$(document).ready(function () {
+
+  $("img.development").click(function () {
+
+    $("#design-description").toggle()
+    $(".development").hide()
+  })
+
+})
+
+
+// Product management description toggle
+$(document).ready(function () {
+
+  $("img.product-management").click(function () {
+
+    $("#product-management-description").toggle()
+    $(".product-management").hide()
+  })
+
+})
+
+
+
