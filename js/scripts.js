c055f32be2b096424fbed7f9908778437400f8d9
wdmuthiora <wdmuthiora@gmail.com>
2021-09-19 12:49:52 +0300

    Added full toggle functionality on the WHAT WE DO section' icons and description.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,68 @@
-// Design description toggle
+// Design description toggle FROM IMAGE TO DESCRIPTION
 $(document).ready(function () {
 
-  $("img.design").click(function () {
+  $("img#design").click(function () {
 
-    $("#design-description").toggle()
+    $("#design-description").show()
     $(".design").hide()
   })
 
 })
 
-// Development desction toggle
+// Development description toggle FROM IMAGE TO DESCRIPTION
 $(document).ready(function () {
 
-  $("img.development").click(function () {
+  $("img#development").click(function () {
 
-    $("#design-description").toggle()
+    $("#development-description").show()
     $(".development").hide()
   })
 
 })
 
 
-// Product management description toggle
+// Product management description toggle FROM IMAGE TO DESCRIPTION
 $(document).ready(function () {
 
-  $("img.product-management").click(function () {
+  $("img#product-management").click(function () {
 
-    $("#product-management-description").toggle()
+    $("#product-management-description").show()
     $(".product-management").hide()
   })
 
 })
 
+// REVERSE
 
+// Design image description toggle FROM DESCRIPTION TO IMAGE
+$(document).ready(function () {
 
+  $("#design-description").click(function () {
+
+    $("#design-description").hide()
+    $(".design").show()
+  })
+
+})
+
+// Development image description toggle FROM DESCRIPTION TO IMAGE
+$(document).ready(function () {
+
+  $("#development-description").click(function () {
+
+    $("#development-description").hide()
+    $(".development").show()
+  })
+
+})
+
+// Product Management image description toggle FROM DESCRIPTION TO IMAGE
+$(document).ready(function () {
+
+  $("#product-management-description").click(function () {
+
+    $("#product-management-description").hide()
+    $(".product-management").show()
+  })
+
+})
